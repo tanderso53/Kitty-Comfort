20b92f0be679c4d082ca550dc7e2c3fb795dc3d1
Tyler Anderson <tasonchou@gmail.com>
2021-03-28 18:05:39 -0500

    Added DataStructure class to log data into and output as JSON

--- sketches/kittycomfort/kittycomfort.ino
+++ sketches/kittycomfort/kittycomfort.ino
@@ -116,6 +116,160 @@ void toggle(bool& reg)
 	else reg = 1;
 }
 
+String jsonBuildHeader()
+{
+	String output = "{\"project\": \"kittycomfort\",";
+	output += "\"sentmillis\": ";
+	output += millis();
+	return output;
+}
+
+String jsonBuildFooter(String eot = String('\n'))
+{
+	String output = "\"EOT\": true}";
+	output += eot;
+	return output;
+}
+
+// Place data stored in memory here for asyncronous upload
+class DataStructure
+{
+ private:
+	size_t _vsize;
+	size_t _size;
+	double* _values;
+	unsigned long* _timemillises;
+	bool* _warmedups;
+
+ public:
+	DataStructure(size_t vsize);
+	DataStructure(const DataStructure& ds);
+	DataStructure(DataStructure&& ds);
+	~DataStructure();
+	void init();
+	void clear();
+	size_t vsize() {return _vsize;}
+	size_t size() {return _size;}
+	double value(unsigned int element);
+	unsigned long timemillis(unsigned int element);
+	bool warmedup(unsigned int element);
+	void addData(double value, unsigned long tmillis, bool warm);
+	String jsonDataString();
+	String jsonFullString();
+};
+
+DataStructure::DataStructure(size_t vsize)
+:_vsize(vsize), _size(0)
+{
+	init();
+}
+
+DataStructure::DataStructure(const DataStructure& ds)
+:_vsize(ds._vsize), _size(ds._size), _values(new double[_vsize]),
+	_timemillises(new unsigned long[_vsize]),
+	_warmedups(new bool[_vsize])
+{
+	for (int i = 0; i < ds._size; i++)
+		{
+			_values[i] = ds._values[i];
+			_timemillises[i] = ds._timemillises[i];
+			_warmedups[i] = ds._warmedups[i];
+		}
+}
+
+DataStructure::DataStructure(DataStructure&& ds)
+:_vsize(ds.vsize()), _size(ds.size()), _values(ds._values),
+	_timemillises(ds._timemillises), _warmedups(ds._warmedups)
+{
+	ds._vsize = 0;
+	ds._size = 0;
+	ds._values = NULL;
+	ds._timemillises = NULL;
+	ds._warmedups = NULL;
+}
+
+DataStructure::~DataStructure()
+{
+	delete [] _values;
+	delete [] _timemillises;
+	delete [] _warmedups;
+}
+
+void DataStructure::init()
+{
+	_values = new double[_vsize];
+	_timemillises = new unsigned long[_vsize];
+	_warmedups = new bool[_vsize];
+}
+
+void DataStructure::clear()
+{
+	delete [] _values;
+	delete [] _timemillises;
+	delete [] _warmedups;
+  _size = 0;
+	init();
+}
+
+inline double DataStructure::value(unsigned int element)
+{
+	if (element < vsize()) return _values[element];
+	else return 0;
+}
+
+inline unsigned long DataStructure::timemillis(unsigned int element)
+{
+	if (element < vsize()) return _timemillises[element];
+	else return 0;
+}
+
+inline bool DataStructure::warmedup(unsigned int element)
+{
+	if (element < vsize()) return _warmedups[element];
+	else return 0;
+}
+
+void DataStructure::addData(double value, unsigned long tmillis,
+														bool warm)
+{
+	if (size() < vsize())
+		{
+			_values[size()] = value;
+			_timemillises[size()] = tmillis;
+			_warmedups[size()] = warm;
+      _size++;
+		}
+}
+
+String DataStructure::jsonDataString()
+{
+	String output = "\"data\": [";
+	for (int i = 0; i < size(); i++)
+		{
+			output += "{\"value\": ";
+			output += value(i);
+			output += ",\"timemillis\": ";
+			output += timemillis(i);
+			output += ",\"iswarmedup\": ";
+      if (warmedup(i)) output += "true";
+			else output += "false";
+			output += "}";
+			if (i < size() -1) output += ",";
+		}
+	output += "]";
+	return output;
+}
+
+String DataStructure::jsonFullString()
+{
+	String output = jsonBuildHeader();
+	output += ",";
+	output += jsonDataString();
+	output += ",";
+	output += jsonBuildFooter();
+	return output;
+}
+
 // Globals
 const int apin = A0;
 const int dpin = 13;
@@ -125,8 +279,11 @@ bool progMode = 0;
 char readBuffer[8];
 //char transBuffer[64];
 unsigned long readoutDelay = 10000; // in ms
+unsigned long transmitDelay = 60000; // in ms
+unsigned long lastTransmit = 0;
 AmmoniaSensor as(apin, dpin);
 SoftwareSerial bt(btrx, bttx);
+DataStructure ds(1 + transmitDelay / readoutDelay);
 
 void setup()
 {
@@ -153,19 +310,26 @@ void loop()
 
 	if (progMode)
 		{
-			if (Serial.available() && bt.available());
+			Serial.println("RAWR");
+      toggle(progMode);
 		}
 						
 	// Read and output data
-	else if (millis() > as.lastRead() + readoutDelay)
+	else
 		{
-			// If time to read, read the ammonia
-			double ammCounts = as.readCounts();
+			if (millis() > as.lastRead() + readoutDelay)
+				{
+					// If time to read, read the ammonia
+					ds.addData(as.readCounts(), millis(), as.isWarmedUp());
+				}
 
-			// If USB is attached, log to USB
-			if (Serial.available()) Serial.println(ammCounts);
-
-			// Print to bluetooth if functional
-			if (bt.available()) bt.println(ammCounts);
+			if (millis() > lastTransmit + transmitDelay)
+				{
+					// Print Json to output
+					if (Serial) Serial.println(ds.jsonFullString());
+					if (bt) bt.print(ds.jsonFullString());
+					ds.clear();
+          lastTransmit = millis();
+				}
 		}
 }
